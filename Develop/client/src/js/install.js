d46403db87a28254241e598cfd163980ad046a74
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-06-21 21:57:37 -0400

    adding install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -13,7 +13,12 @@ butInstall.addEventListener("click", async () => {
   if (!promptClick) {
     return;
   }
+  promptClick.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
+});
